4fab33ac59b94a89d1aedd02d56ee51d9b8ccd2f
luquita78 <lucasbarbosa.oliveira78@gmail.com>
2023-05-18 15:59:51 -0300

    header

---
+++ src/components/IconesHeader/index.js
@@ -0,0 +1,15 @@
+import './style.css'
+import perfil from '../../imagens/perfil.svg'
+import sacola from '../../imagens/sacola.svg'
+
+
+const icones = [perfil, sacola]
+
+function IconeHeader (){
+    return(
+        <ul className='icones'>
+            {icones.map ((icone) => <li className='icone'><img src={icone}></img></li>)}
+        </ul>
+    )
+}
+export default IconeHeader
