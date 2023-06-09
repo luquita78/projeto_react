70bb9bb9edaacb5f58f1df4c52b353526b026c60
luquita78 <lucasbarbosa.oliveira78@gmail.com>
2023-06-09 12:03:05 -0300

    input de pesquisa

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Header from './components/Header'
 import styled from 'styled-components'
-
+import Pesquisa from './components/Pesquisa';
 
 const AppContainer =styled.div`
     width: 100vw;
@@ -15,6 +15,7 @@ function App() {
   return (
     <AppContainer>
       <Header/>
+      <Pesquisa/>
     </AppContainer>
   );
 }
